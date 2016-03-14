b0b44a514a9c742ad463e31e85c2f4399c08004e
Alexander Seliverstov <seliverstov.a@gmail.com>
2016-03-14 15:47:20 +0300

    Add ORM mapping for images
    Add database annotations for Images.Img class
    Add new method to ORMUtils for loading images data to database

--- src/main/java/ORMUtils.java
+++ src/main/java/ORMUtils.java
@@ -1,15 +1,15 @@
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
-import xml.ANN;
-import xml.Anime;
-import xml.Manga;
-import xml.Titles;
+import xml.*;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Collection;
 
 /**
  * Created by a.g.seliverstov on 11.03.2016.
@@ -29,7 +29,6 @@ public class ORMUtils {
         Titles titles = XMLUtils.parseTitles(new File(TITLES_FILE_PATH));
         ConnectionSource connectionSource = new JdbcConnectionSource(DATABASE_CONNECTION_URL);
 
-//        TableUtils.createTable(connectionSource, Titles.Item.class);
 
         /*TableUtils.createTable(connectionSource, Manga.class);
         TableUtils.createTable(connectionSource, Manga.Credit.class);
@@ -42,68 +41,106 @@ public class ORMUtils {
         TableUtils.createTable(connectionSource, Anime.Episode.class);
         TableUtils.createTable(connectionSource, Anime.Episode.Title.class);
         TableUtils.createTable(connectionSource, Anime.Cast.class);*/
+        TableUtils.createTable(connectionSource, Images.Img.class);
 
-        for(Titles.Item t: titles.items) {
-            if ("5445".equals(t.id) || "16857".equals(t.id)) {
-                try {
-                    String path = String.format(ITEM_FILE_PATH_TEMPLATE,t.id,t.id);
-                    ANN ann = XMLUtils.parseANN(new File(path));
-                    System.out.println(ann);
-                    if (ann.anime!=null){
-                        saveManga(ann.anime, connectionSource);
-                    }
-                    if (ann.manga!=null){
-                        saveManga(ann.manga, connectionSource);
-                    }
-                }catch (Exception e){
-                    System.out.println(t.id+","+t.type+" - error: ");
-                    e.printStackTrace();
+       /* for(Titles.Item t: titles.items) {
+            try {
+                String path = String.format(ITEM_FILE_PATH_TEMPLATE,t.id,t.id);
+                ANN ann = XMLUtils.parseANN(new File(path));
+                Images images = XMLUtils.parseImages(new File(path));
+                System.out.println(ann);
+                if (ann.anime!=null){
+                    saveManga(ann.anime, connectionSource);
                 }
+                if (ann.manga!=null){
+                    saveManga(ann.manga, connectionSource);
+                }
+            }catch (Exception e){
+                System.out.println(t.id+","+t.type+" - error: ");
+                e.printStackTrace();
             }
+        }*/
+        for(Titles.Item t: titles.items) {
+            try {
+                String path = String.format(ITEM_FILE_PATH_TEMPLATE,t.id,t.id);
+                Images images = XMLUtils.parseImages(new File(path));
+                System.out.println(images);
+                saveImages(images, connectionSource);
+            }catch (Exception e){
+                System.out.println(t.id+","+t.type+" - error: ");
+                e.printStackTrace();
+            }
+
         }
         connectionSource.close();
     }
 
+    public static void saveImages(Images images, ConnectionSource connectionSource) throws SQLException {
+        Dao<Images.Img,Integer> imagesDao = DaoManager.createDao(connectionSource, Images.Img.class);
+
+        Dao<Manga, Integer> mangaDao = DaoManager.createDao(connectionSource, Manga.class);
+
+        Integer annId = null;
+        Collection<Images.Img> imagesCollection = null;
+        if (images.animeId!=0) {
+            annId=images.animeId;
+            imagesCollection = images.animeImages;
+        }else if (images.mangaId!=0) {
+            annId=images.mangaId;
+            imagesCollection = images.mangaImages;
+        }
+        if (annId!=null && imagesCollection!=null){
+            for(Images.Img i:imagesCollection){
+                QueryBuilder<Manga,Integer> qb = mangaDao.queryBuilder();
+                qb.where().eq("annId",annId);
+                PreparedQuery<Manga> pq = qb.prepare();
+                Manga manga = mangaDao.queryForFirst(pq);
+                i.manga = manga;
+                imagesDao.create(i);
+            }
+        }
+    }
+
     public static void saveManga(Manga manga, ConnectionSource connectionSource) throws SQLException {
-        Dao<Manga,String> mangaDao = DaoManager.createDao(connectionSource, Manga.class);
+        Dao<Manga,Integer> mangaDao = DaoManager.createDao(connectionSource, Manga.class);
         mangaDao.create(manga);
         if (manga.staff!=null) {
-            Dao<Manga.Staff, String> mangaStaffDao = DaoManager.createDao(connectionSource, Manga.Staff.class);
+            Dao<Manga.Staff, Integer> mangaStaffDao = DaoManager.createDao(connectionSource, Manga.Staff.class);
             for (Manga.Staff s : manga.staff) {
                 s.manga=manga;
                 mangaStaffDao.create(s);
             }
         }
         if (manga.reviews!=null) {
-            Dao<Manga.Review, String> mangaReviewDao = DaoManager.createDao(connectionSource, Manga.Review.class);
+            Dao<Manga.Review, Integer> mangaReviewDao = DaoManager.createDao(connectionSource, Manga.Review.class);
             for (Manga.Review s : manga.reviews) {
                 s.manga=manga;
                 mangaReviewDao.create(s);
             }
         }
         /*if (manga.releases!=null) {
-            Dao<Manga.Release, String> mangaReleaseDao = DaoManager.createDao(connectionSource, Manga.Release.class);
+            Dao<Manga.Release, Integer> mangaReleaseDao = DaoManager.createDao(connectionSource, Manga.Release.class);
             for (Manga.Release s : manga.releases) {
                 s.manga=manga;
                 mangaReleaseDao.create(s);
             }
         }*/
         if (manga.related!=null) {
-            Dao<Manga.Related, String> mangaRelatedDao = DaoManager.createDao(connectionSource, Manga.Related.class);
+            Dao<Manga.Related, Integer> mangaRelatedDao = DaoManager.createDao(connectionSource, Manga.Related.class);
             for (Manga.Related s : manga.related) {
                 s.manga=manga;
                 mangaRelatedDao.create(s);
             }
         }
         if (manga.credits!=null) {
-            Dao<Manga.Credit, String> mangaCreditDao = DaoManager.createDao(connectionSource, Manga.Credit.class);
+            Dao<Manga.Credit, Integer> mangaCreditDao = DaoManager.createDao(connectionSource, Manga.Credit.class);
             for (Manga.Credit s : manga.credits) {
                 s.manga=manga;
                 mangaCreditDao.create(s);
             }
         }
         if (manga.info!=null) {
-            Dao<Manga.Info, String> mangaInfoDao = DaoManager.createDao(connectionSource, Manga.Info.class);
+            Dao<Manga.Info, Integer> mangaInfoDao = DaoManager.createDao(connectionSource, Manga.Info.class);
             for (Manga.Info s : manga.info) {
                 s.manga=manga;
                 mangaInfoDao.create(s);
@@ -118,13 +155,13 @@ public class ORMUtils {
         if (manga instanceof Anime){
             Anime anime = (Anime)manga;
             if (anime.episodes!=null) {
-                Dao<Anime.Episode, String> animeEpisodesDao = DaoManager.createDao(connectionSource, Anime.Episode.class);
+                Dao<Anime.Episode, Integer> animeEpisodesDao = DaoManager.createDao(connectionSource, Anime.Episode.class);
                 for (Anime.Episode s : anime.episodes) {
                     s.manga=manga;
                     animeEpisodesDao.create(s);
 
                     if (s.titles!=null) {
-                        Dao<Anime.Episode.Title, String> animeEpisodeTitlesDao = DaoManager.createDao(connectionSource, Anime.Episode.Title.class);
+                        Dao<Anime.Episode.Title, Integer> animeEpisodeTitlesDao = DaoManager.createDao(connectionSource, Anime.Episode.Title.class);
                         for (Anime.Episode.Title t : s.titles) {
                             t.episode = s;
                             animeEpisodeTitlesDao.create(t);
@@ -134,7 +171,7 @@ public class ORMUtils {
                 }
             }
             /*if (anime.casts!=null) {
-                Dao<Anime.Cast, String> animeCastsDao = DaoManager.createDao(connectionSource, Anime.Cast.class);
+                Dao<Anime.Cast, Integer> animeCastsDao = DaoManager.createDao(connectionSource, Anime.Cast.class);
                 for (Anime.Cast s : anime.casts) {
                     s.manga=manga;
                     animeCastsDao.create(s);
--- src/main/java/xml/Images.java
+++ src/main/java/xml/Images.java
@@ -1,5 +1,7 @@
 package xml;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 import org.simpleframework.xml.*;
 
 import java.util.Collection;
@@ -8,42 +10,53 @@ import java.util.List;
 /**
  * Created by a.g.seliverstov on 10.03.2016.
  */
+
 @Root(strict = false)
 public class Images {
     @Attribute(name = "id", required = false)
     @Path("manga")
-    public String mangaId;
+    public int mangaId;
 
     @Attribute(name = "id", required = false)
     @Path("anime")
-    public String animeId;
+    public int animeId;
 
     @ElementList(entry = "img", required = false, inline = true, type = Img.class)
     @Path("manga/info")
-    public List<Img> mangaImages;
+    public Collection<Img> mangaImages;
 
     @ElementList(entry = "img", required = false, inline = true, type = Img.class)
     @Path("anime/info")
     public Collection<Img> animeImages;
 
+    @DatabaseTable(tableName = "images")
     @Root
     public static class Img {
+        @DatabaseField(generatedId = true)
+        public int id;
+
+        @DatabaseField(foreign = true, foreignAutoCreate = true)
+        public Manga manga;
+
+        @DatabaseField
         @Attribute
         public String src;
 
+        @DatabaseField
         @Attribute
-        public String width;
+        public int width;
 
+        @DatabaseField
         @Attribute
-        public String height;
+        public int height;
     }
 
     @Override
     public String toString() {
-        if (mangaId!=null){
+        if (mangaId!=0){
             return "Manga " + mangaId+" has "+((mangaImages==null)?0:mangaImages.size())+" images";
         }
-        if (animeId!=null){
+        if (animeId!=0){
             return "Anime " + animeId+" has "+((animeImages==null)?0:animeImages.size())+" images";
         }
         return super.toString();
